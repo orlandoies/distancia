fe3abc894e447211588558c22215505c41e57451
orlandoies <99322418+orlandoies@users.noreply.github.com>
2022-03-03 14:22:41 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 39)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 5 && maqueen.Ultrasonic(PingUnit.Centimeters) <= 8) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 24)
+    }
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 5) {
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 10)
+    }
+})
